780f4324825416952fb4dd57f493e916d8eae7ed
kalyyan <Kalyyan.2612@gmail.com>
2020-06-08 20:11:20 +0530

    daily changes

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,36 +1,37 @@
-import React from 'react';  
+import React, { useEffect } from "react";
 
-import classes from './Cockpit.css';
+import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log("[cockpit.js] useEffect");
+  });
 
-    const assignedClasses = [];
+  const assignedClasses = [];
 
-    let btnClass = '';
-    
+  let btnClass = "";
 
-    if(props.showPersons) {
-        btnClass = classes.Red;
-    }
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
 
-    if( props.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
 
-    if( props.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
 
-
-    return (
-        <div className={classes.Cockpit} > 
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button 
-                className={btnClass}
-                onClick={props.clicked}>Toggle person</button>
-        </div>      
-    );
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle person
+      </button>
+    </div>
+  );
 };
 
 export default cockpit;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,46 +1,61 @@
-import React, { Component } from 'react';
-import classes from './App.css';
-import Persons from '../Components/Persons/Persons';
-import Cockpit from '../Components/Cockpit/Cockpit';
+import React, { Component } from "react";
+import classes from "./App.css";
+import Persons from "../Components/Persons/Persons";
+import Cockpit from "../Components/Cockpit/Cockpit";
 
 // import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
-
 class App extends Component {
-  state = {
-    persons: [
-      { id: "asfa1", name: 'Max', age: 28 },
-      { id: "kdsd1", name: 'Manu', age: 29 },
-      { id: "sdsc1", name: 'stephenie', age: 26 }
-    ],
-    otherState : "Some other value",
-    showPersons: false
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
   }
 
+  state = {
+    persons: [
+      { id: "asfa1", name: "Max", age: 28 },
+      { id: "kdsd1", name: "Manu", age: 29 },
+      { id: "sdsc1", name: "stephenie", age: 26 },
+    ],
+    otherState: "Some other value",
+    showPersons: false,
+  };
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
 
-  
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] conponentDidUpdte");
+  }
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
-
-
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
-
-
+    this.setState({ showPersons: !doesShow });
+  };
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id = id;
+    const personIndex = this.state.persons.findIndex((p) => {
+      return (p.id = id);
     });
 
-    const person = {...this.state.persons[personIndex]};
+    const person = { ...this.state.persons[personIndex] };
 
     // const person = Object.assign({}, this.state.persons[personIndex]);
 
@@ -49,35 +64,37 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons })
-  }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    this.setState({ persons: persons });
+  };
 
   render() {
-  
-  let persons = null;
+    console.log("[App.js] render");
 
-  if(this.state.showPersons) {
-    persons = 
-                  <Persons 
-                    
-                    persons={this.state.persons}
-                    clicked={this.deletePersonHandler}
-                    changed={this.nameChangedHandler} />;
-  }
+    let persons = null;
 
-    
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
+    }
 
     return (
-        <div className={classes.App}>
-            <Cockpit title={this.props.appTitle} showPersons={this.state.showPersons} persons={this.state.persons} clicked={this.togglePersonsHandler} />
-            {persons}           
-        </div>
+      <div className={classes.App}>
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+        {persons}
+      </div>
     );
     // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi! I am a react app'))
   }
 }
 
 export default App;
-
-
